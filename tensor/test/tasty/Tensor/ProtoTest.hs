6a1eac78a97312ebd64ed436c1baea4f5756de3f
Ruben Astudillo <ruben.astud@gmail.com>
2020-11-26 11:27:34 -0300

    Rework hspec format to the standard on their docs
    It required rewording so the list structure presented on the logs wouldn't
    be as redundant.

--- tensor/test/tasty/Tensor/ProtoTest.hs
+++ tensor/test/tasty/Tensor/ProtoTest.hs
@@ -8,17 +8,19 @@ import Test.Tasty.Hspec
 tests :: Spec
 tests = do
   describe "Tensor.List" $ do
-    it "fromList: bigger declared Dims than size of list." $
-      fromList [5, 2] [(1 :: Float) .. 4] `shouldBe` Nothing
-    it "fromList: lesser declared Dims than size of list." $
-      let val = fromList [3, 2] [(1 :: Float) .. 7]
-          target = Tensor [3,2] EltFloat [1 .. 6]
-      in val `shouldBe` (Just target)
-    it "fromList: correct Dims for passed list." $
-      let val = fromList [2, 2] [(1 :: Float) .. 4]
-          target = Tensor [2, 2] EltFloat [(1 :: Float), 2, 3, 4]
-       in val `shouldBe` (Just target)
-    it "Dimensions preserved on fill" $
-      let val = fill [3, 2] (pi :: Double)
-          target = Tensor [3, 2] EltDouble [pi, pi, pi, pi, pi, pi]
-       in val `shouldBe` target
+    describe "fromList" $ do
+      it "fails when passing fewer elements than declared on the `Dims`" $
+        fromList [5, 2] [(1 :: Float) .. 4] `shouldBe` Nothing
+      it "creates a tensor when passing strictly more elements than declared on the `Dims`" $
+        let val = fromList [3, 2] [(1 :: Float) .. 7]
+            target = Tensor [3, 2] EltFloat [1 .. 6]
+         in val `shouldBe` (Just target)
+      it "creates a tensor when passing exactly the number of elements declared on the `Dims`" $
+        let val = fromList [2, 2] [(1 :: Float) .. 4]
+            target = Tensor [2, 2] EltFloat [(1 :: Float), 2, 3, 4]
+         in val `shouldBe` (Just target)
+    describe "fill" $ do
+      it "preserves dimensions" $
+        let val = fill [3, 2] (pi :: Double)
+            target = Tensor [3, 2] EltDouble [pi, pi, pi, pi, pi, pi]
+         in val `shouldBe` target
