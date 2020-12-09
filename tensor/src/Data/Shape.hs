630a087b33d47ca0fd3fdffd4ff685258c753673
Viktor Kronvall <viktor.kronvall@gmail.com>
2020-12-09 21:30:14 +0900

    Support label batches in smooth-grad

--- tensor/src/Data/Shape.hs
+++ tensor/src/Data/Shape.hs
@@ -5,9 +5,11 @@ module Data.Shape
   , Shape(..)
   , dimsSize
   , unifyShapes
+  , unifyDims
   )
 where
 
+import           Control.Monad             (guard)
 import           Data.Binary
 import           Data.Positive
 import           Data.Text.Prettyprint.Doc
@@ -49,3 +51,25 @@ unifyShapes sa@(Shape da ba) sb@(Shape db bb) = Shape da <$>
       | otherwise -> pure $ Just b
     (Nothing,b) -> pure b
     (b,Nothing) -> pure b
+
+-- | Does the `Dims` satisfy the constraints imposed by the `Shape`? If so,
+-- return the expected `Dims` adapted to fit the structure of the `Shape`.
+unifyDims :: Shape -> Dims -> Either String Dims
+unifyDims sh@(Shape ds batch) dims
+  | ds == dims = pure dims
+  | ds == ds' = do
+    unifyBatch batch b' -- compare batch sizes
+    pure (ds <> [b'])
+  | otherwise = Left $ unwords
+    [ "Cannot unify dimensions"
+    , show dims
+    , "with shape"
+    , show sh
+    ]
+    where
+      ds' = take (length ds) dims
+      -- Multiply remaining dimensions into the "batch size" expected by the
+      -- shape
+      b' = product $ drop (length ds) dims
+      unifyBatch Nothing _  = pure ()
+      unifyBatch (Just a) x = guard $ a == x
