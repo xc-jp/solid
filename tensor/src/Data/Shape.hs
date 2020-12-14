03ce0cb24c2df15710badf0e12adb50e68cccccc
Viktor Kronvall <viktor.kronvall@gmail.com>
2020-12-14 23:47:14 +0900

    Update Shape definition
    c319a32 Update Shape definition
    729dd5b WIP: Shapes with clearer semantics
    242b84c Update valholl test shapes
    b4ed2ee Fix valholl-caffe2 tests
    acec76a Passes most tests
    52c3d00 Fix protobuf parsing bug
    93b4ded Update Shape type in grotti executable
    0a822b7 Note about valholl protobuf backwards-compatiblity
    332252f Update shapes in cudnn test
    e82da70 Convert shapes to dims in cudnn test
    dbaa5d4 Update shapes in example networks
    ff327f4 Update vvm benchmark
    85d3453 Update Shape type in NFData instance
    97fe3de Update shape type in vvm benchmark
    eebd150 Drop type parameter from `Shape`
    ec948ce Example for `prefixShape`
    2528a5a Update Shape Show instance
    0a82ca4 Note about use of 'Dims' in valhollvm
    6f28575 Support legacy shape protobuf
    a311dc6 Specify FC semantics in more detail
    ee43bcd Format with stylish-haskell
    412d4b0 Rename `l` to `axis`
    a7fc052 Expand FC tests given FCMode semantics
    aabae99 No need to reshape before FC
    a790486 Rename Shape functions
    6248999 Paths_treyja is for the executable
    75b7ab1 Update valholl/src/Valholl/Data/ValhollOp/Verification.hs
    b0a4bfc Update valholl/src/Valholl/Data/ValhollOp/Verification.hs
    65f7366 Documentation
    0c56acd Update operator name for prefixShape
    3ba3d31 Simplify classes shape
    
    Update operator name for prefixShape
    
    Simplify classes shape

--- tensor/src/Data/Shape.hs
+++ tensor/src/Data/Shape.hs
@@ -6,10 +6,15 @@ module Data.Shape
   , dimsSize
   , unifyShapes
   , unifyDims
+  , toDims
+  , fromDims
+  , splitDims
+  , broadcastShapes
+  , prefixShape
+  , (#:.)
   )
 where
 
-import           Control.Monad             (guard)
 import           Data.Positive
 import           Data.Text.Prettyprint.Doc
 import           GHC.Generics              (Generic)
@@ -20,53 +25,102 @@ dimsSize :: Dims -> Int
 dimsSize = product . fmap fromIntegral
 
 -- | Shape, potentially annotated with a batch size.
---   Shapes are reversed; i.e. the fastest changing element comes first.
-data Shape = Shape
-  { shDims  :: Dims
-  , shBatch :: Maybe Positive
-  }
-  deriving (Eq, Show, Generic)
+--   Shapes are little-endian; i.e. the fastest changing dimension comes first.
+data Shape =
+  Positive :. Shape
+  | Z
+  | Any
+  deriving (Generic, Eq)
+
+instance Show Shape where
+  show (x :. Z)  = show x
+  show (x :. xs) = show x <> "x" <> show xs
+  show Any       = "?"
+  show Z         = "Z" -- scalar
+
+infixr 6 :.
 
 instance Pretty Shape where
-  pretty (Shape dims b) = concatWith (\x y -> x <> pretty "×" <> y) (prettyBatch b : fmap pretty (reverse dims))
-    where
-    prettyBatch :: Maybe Positive -> Doc ann
-    prettyBatch Nothing  = pretty "?"
-    prettyBatch (Just b) = pretty b
+  pretty (x :. Z)  = pretty x
+  pretty (x :. xs) = pretty x <> pretty "×" <> pretty xs
+  pretty Any       = pretty "?"
+  pretty Z         = pretty "Z"
 
 -- | Find, if possible, the most general shape that matches both arguments.
-unifyShapes
-  :: Shape
-  -> Shape
-  -> Either String Shape
-unifyShapes sa@(Shape da ba) sb@(Shape db bb) = Shape da <$>
-  case (ba,bb) of
-    _ | da /= db -> Left $ unwords
-        [ "Cannot unify shapes ", show sa , "and", show sb <> "; different dimensions" ]
-    (Just b,Just b')
-      | b /= b' -> Left "Batches differ"
-      | otherwise -> pure $ Just b
-    (Nothing,b) -> pure b
-    (b,Nothing) -> pure b
+unifyShapes :: Shape -> Shape -> Maybe Shape
+unifyShapes Z Z        = pure Z
+unifyShapes Any ys     = pure ys
+unifyShapes xs Any     = pure xs
+unifyShapes Z (_ :. _) = Nothing
+unifyShapes (_ :. _) Z = Nothing
+unifyShapes (x :. xs) (y :. ys)
+  | x == y = fmap (x :.) (unifyShapes xs ys)
+  | otherwise = Nothing
 
--- | Does the `Dims` satisfy the constraints imposed by the `Shape`? If so,
--- return the expected `Dims` adapted to fit the structure of the `Shape`.
-unifyDims :: Shape -> Dims -> Either String Dims
-unifyDims sh@(Shape ds batch) dims
-  | ds == dims = pure dims
-  | ds == ds' = do
-    unifyBatch batch b' -- compare batch sizes
-    pure (ds <> [b'])
-  | otherwise = Left $ unwords
-    [ "Cannot unify dimensions"
-    , show dims
-    , "with shape"
-    , show sh
-    ]
-    where
-      ds' = take (length ds) dims
-      -- Multiply remaining dimensions into the "batch size" expected by the
-      -- shape
-      b' = product $ drop (length ds) dims
-      unifyBatch Nothing _  = pure ()
-      unifyBatch (Just a) x = guard $ a == x
+-- | Split a 'Shape' at an index into the 'Dims' up until that index and the
+-- remaining 'Shape' from that index. If less dims than the given index are
+-- known this fails with 'Nothing'.
+splitDims :: Int -> Shape -> Maybe (Dims, Shape)
+splitDims 0 z         = pure ([], z)
+splitDims n (x :. xs)
+  | n >= 1 = do
+    (ds,z) <- splitDims (pred n) xs
+    pure (x : ds, z)
+  | otherwise = Nothing
+splitDims _ _ = Nothing
+
+-- | Broadcast shapes together.
+--
+-- We borrow broadcasting semantics from numpy.
+-- https://numpy.org/doc/stable/user/basics.broadcasting.html#general-broadcasting-rules
+--
+-- The broadcasting is bi-directional and the resulting shape has the maximum
+-- rank of the two arguments.
+broadcastShapes :: Shape -> Shape -> Maybe Shape
+broadcastShapes Z ys                = pure ys
+broadcastShapes xs Z                = pure xs
+broadcastShapes Any ys              = pure ys
+broadcastShapes xs Any              = pure xs
+broadcastShapes (1 :. xs) (y :. ys) = (y :.) <$> broadcastShapes xs ys
+broadcastShapes (x :. xs) (1 :. ys) = (x :.) <$> broadcastShapes xs ys
+broadcastShapes (x :. xs) (y :. ys)
+  | x == y = (x :.) <$> broadcastShapes xs ys
+  | otherwise = Nothing
+
+-- | Convert 'Shape' to 'Dims' by replacing batch 'Any' with trailing 'Dims'.
+toDims :: Dims -> Shape -> Dims
+toDims _ Z         = []
+toDims z Any       = z
+toDims z (x :. xs) = x : toDims z xs
+
+-- | Convert 'Dims' to known size 'Shape'
+fromDims :: Dims -> Shape
+fromDims []       = Z
+fromDims (x : xs) = x :. fromDims xs
+
+-- | Prefix a shape with inner-most dimensions
+-- Example:
+-- >>> prefixShape [3,4,5] (2 :. Z)
+-- 3x4x5x2
+prefixShape :: Dims -> Shape -> Shape
+prefixShape ds = go (reverse ds)
+  where
+    go (x:xs) b = go xs (x :. b)
+    go []     b = b
+
+-- | Infix operator for 'prefixShape'
+(#:.) :: Dims -> Shape -> Shape
+(#:.) = prefixShape
+
+infixr 4 #:.
+
+-- | Unify dimensions with shape returning matched dimensions and dimensions of
+-- the optional Any tail.
+unifyDims :: Shape -> Dims -> Either String (Dims, Dims)
+unifyDims Z [] = pure ([], [])
+unifyDims Z _ = Left $ unwords ["Too many dimensions"]
+unifyDims (_ :. _) [] = Left $ unwords ["Too few dimensions"]
+unifyDims (x :. xs) (y : ys)
+  | x == y = fmap (\(as,bs) -> (x:as, bs)) (unifyDims xs ys)
+  | otherwise = Left $ unwords ["Dimensions", show x, "and", show y, "do not match"]
+unifyDims Any{} ys = pure ([], ys)
