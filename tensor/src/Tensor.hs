d68b266faa073b3f1d4f87ae1fd608ab15bbca89
Jonas Carpay <jonascarpay@gmail.com>
2021-02-10 14:18:05 +0900

    More auxiliary functions

--- tensor/src/Tensor.hs
+++ tensor/src/Tensor.hs
@@ -16,21 +16,28 @@ module Tensor
     Dynamic (..),
     dynamic,
     dynamic',
+    dynamic_,
+    bimapDynamic,
+    bimapDynamic_,
     hmapDynamic,
     htraverseDynamic,
     DSTensor,
     DUTensor,
     DVTensor,
     DLTensor,
+    dtensorDims,
+    dtensorElt,
 
     -- * Scalar
+    Scalar,
+    scalar,
     Identity (..),
     pattern SFloat,
     pattern SInt,
 
     -- * Elt
     Elt,
-    dynElt,
+    toElt,
     pattern EltFloat,
     pattern EltInt,
     Proxy (..),
@@ -57,9 +64,19 @@ deriving instance (Eq (f Float), Eq (f Int)) => Eq (Dynamic f)
 
 deriving instance (Show (f Float), Show (f Int)) => Show (Dynamic f)
 
+dtensorDims :: Dynamic (Tensor f) -> Dims
+dtensorDims = dynamic' tensorDims
+
+dtensorElt :: Dynamic (Tensor f) -> Elt
+dtensorElt = toElt
+
 dynamic :: (f Float -> r) -> (f Int -> r) -> (Dynamic f -> r)
 dynamic f _ (DFloat v) = f v
 dynamic _ f (DInt v) = f v
+
+dynamic_ :: r -> r -> (Dynamic f -> r)
+dynamic_ r _ (DFloat _) = r
+dynamic_ _ r (DInt _) = r
 
 dynamic' :: (forall a. f a -> r) -> (Dynamic f -> r)
 dynamic' f (DFloat v) = f v
@@ -68,6 +85,14 @@ dynamic' f (DInt v) = f v
 hmapDynamic :: (forall a. f a -> g a) -> Dynamic f -> Dynamic g
 hmapDynamic f (DFloat v) = DFloat $ f v
 hmapDynamic f (DInt v) = DInt $ f v
+
+bimapDynamic :: (f Float -> g Float) -> (f Int -> g Int) -> Dynamic f -> Dynamic g
+bimapDynamic f _ (DFloat v) = DFloat $ f v
+bimapDynamic _ f (DInt v) = DInt $ f v
+
+bimapDynamic_ :: f Float -> f Int -> Dynamic g -> Dynamic f
+bimapDynamic_ r _ (DFloat _) = DFloat r
+bimapDynamic_ _ r (DInt _) = DInt r
 
 htraverseDynamic :: Functor m => (forall a. f a -> m (g a)) -> Dynamic f -> m (Dynamic g)
 htraverseDynamic f (DFloat v) = DFloat <$> f v
@@ -83,14 +108,20 @@ type DVTensor = Dynamic VTensor
 
 type Elt = Dynamic Proxy
 
-dynElt :: Dynamic f -> Elt
-dynElt = hmapDynamic (const Proxy)
+toElt :: Dynamic f -> Elt
+toElt = bimapDynamic_ Proxy Proxy
 
 pattern EltFloat, EltInt :: Elt
 pattern EltFloat = DFloat Proxy
 pattern EltInt = DInt Proxy
 
+{-# COMPLETE EltFloat, EltInt #-}
+
 type Scalar = Dynamic Identity
+
+scalar :: (Float -> r) -> (Int -> r) -> Scalar -> r
+scalar f _ (DFloat (Identity x)) = f x
+scalar _ f (DInt (Identity x)) = f x
 
 pattern SFloat :: Float -> Scalar
 pattern SFloat f = DFloat (Identity f)
