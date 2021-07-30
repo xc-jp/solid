59ff228d8951af6c85417168cde95c1846c2f09f
Jonas Carpay <jonascarpay@gmail.com>
2021-07-30 20:34:50 +0900

    WIP

--- tensor/test/tasty/Tensor/ProtoTest.hs
+++ tensor/test/tasty/Tensor/ProtoTest.hs
@@ -1,6 +1,5 @@
 module Tensor.ProtoTest (tests) where
 
-import Data.Elt
 import Tensor.Common
 import Tensor.List
 import Test.Tasty.Hspec
@@ -13,14 +12,14 @@ tests = do
         fromList [5, 2] [(1 :: Float) .. 4] `shouldBe` Nothing
       it "creates a tensor when passing strictly more elements than declared on the `Dims`" $
         let val = fromList [3, 2] [(1 :: Float) .. 7]
-            target = Tensor [3, 2] EltFloat [1 .. 6]
+            target = Tensor [3, 2] [1 .. 6]
          in val `shouldBe` Just target
       it "creates a tensor when passing exactly the number of elements declared on the `Dims`" $
         let val = fromList [2, 2] [(1 :: Float) .. 4]
-            target = Tensor [2, 2] EltFloat [1 :: Float, 2, 3, 4]
+            target = Tensor [2, 2] [1 :: Float, 2, 3, 4]
          in val `shouldBe` Just target
     describe "fill" $ do
       it "preserves dimensions" $
         let val = fill [3, 2] (pi :: Double)
-            target = Tensor [3, 2] EltDouble [pi, pi, pi, pi, pi, pi]
+            target = Tensor [3, 2] [pi, pi, pi, pi, pi, pi]
          in val `shouldBe` target
