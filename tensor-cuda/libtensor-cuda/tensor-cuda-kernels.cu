2dd3a0b0519b57db0409fac2d7a25dccbbbbd0b2
crvdgc <ubikium@gmail.com>
2021-10-26 16:12:38 +0900

    add const for C&C++ code

--- tensor-cuda/libtensor-cuda/tensor-cuda-kernels.cu
+++ tensor-cuda/libtensor-cuda/tensor-cuda-kernels.cu
@@ -2,29 +2,31 @@
 #include <cstdio>
 #include "tensor-cuda-kernels.cuh"
 
-#define checkCudaErr(...)        \
-  {                              \
-    int err = (int) __VA_ARGS__; \
-    if (err)                     \
-      return err;                \
+#define checkCudaErr(...)       \
+  {                             \
+    int err = (int)__VA_ARGS__; \
+    if (err)                    \
+      return err;               \
   }
 
-int tensor_cuda::devMalloc(size_t bytes, void **p) {
+int tensor_cuda::devMalloc(const size_t bytes, void** p) {
   checkCudaErr(cudaMalloc(p, bytes));
   return 0;
 }
 
-int tensor_cuda::devFree(void *devp) {
+int tensor_cuda::devFree(void* devp) {
   checkCudaErr(cudaFree(devp));
   return 0;
 }
 
-int tensor_cuda::memcpyToDev(size_t bytes, void *devDst, void *hostSrc) {
+int tensor_cuda::memcpyToDev(const size_t bytes, void* devDst, void* hostSrc) {
   checkCudaErr(cudaMemcpy(devDst, hostSrc, bytes, cudaMemcpyHostToDevice));
   return 0;
 }
 
-int tensor_cuda::memcpyFromDev(size_t bytes, void *hostDst, void *devSrc) {
+int tensor_cuda::memcpyFromDev(const size_t bytes,
+                               void* hostDst,
+                               void* devSrc) {
   checkCudaErr(cudaMemcpy(hostDst, devSrc, bytes, cudaMemcpyDeviceToHost));
   return 0;
 }
--- tensor-cuda/libtensor-cuda/tensor-cuda.cpp
+++ tensor-cuda/libtensor-cuda/tensor-cuda.cpp
@@ -1,14 +1,14 @@
 #include "tensor-cuda.h"
 #include "tensor-cuda-kernels.cuh"
-int devMalloc(size_t bytes, void** p) {
+int devMalloc(const size_t bytes, void** p) {
   return tensor_cuda::devMalloc(bytes, p);
 }
 int devFree(void* devp) {
   return tensor_cuda::devFree(devp);
 }
-int memcpyToDev(size_t bytes, void* devDst, void* hostSrc) {
+int memcpyToDev(const size_t bytes, void* devDst, void* hostSrc) {
   return tensor_cuda::memcpyToDev(bytes, devDst, hostSrc);
 }
-int memcpyFromDev(size_t bytes, void* hostDst, void* devSrc) {
+int memcpyFromDev(const size_t bytes, void* hostDst, void* devSrc) {
   return tensor_cuda::memcpyFromDev(bytes, hostDst, devSrc);
 }
