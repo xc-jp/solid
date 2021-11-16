0e373c138fe08404b5cf0e36fbd5d150f1a4d34b
Jonas Carpay <jonascarpay@gmail.com>
2021-11-16 21:53:43 +0900

    Fix CUDA build

--- tensor-cuda/libtensor-cuda/default.nix
+++ tensor-cuda/libtensor-cuda/default.nix
@@ -1,6 +1,11 @@
-{ stdenv, cmake, cudart, cudatoolkit }:
+{ stdenv, cmake, cudart, cudatoolkit, addOpenGLRunpath }:
 stdenv.mkDerivation {
   src = ./.;
   buildInputs = [ cmake cudart cudatoolkit ];
+  nativeBuildInputs = [ addOpenGLRunpath ];
   name = "libtensor-cuda";
+  # TODO explain this voodoo
+  postFixup = ''
+    addOpenGLRunpath $out/lib/libtensor-cuda.so
+  '';
 }
